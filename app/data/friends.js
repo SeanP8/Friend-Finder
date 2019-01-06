5ccf7ab89e080ae2508799cf3907477501c0ebd8
Sean <seanpitfield@gmail.com>
2019-01-05 22:23:31 -0800

    having a typeError cannot read property '0' of undefined

---
+++ app/data/friends.js
@@ -0,0 +1,28 @@
+let friends = [
+  {
+    name: "Hulk",
+    photo:
+      "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjm4ZCOodjfAhXAGTQIHSYpBjIQjRx6BAgBEAU&url=https%3A%2F%2Fwww.maxpixel.net%2FJapan-Hulk-Marvel-2697106&psig=AOvVaw1Uoae9j20-FSZcP09O_3sE&ust=1546833219656876",
+    score: ["1", "4", "2", "5", "5", "3", "2", "1", "5", "2"]
+  },
+  {
+    name: "Captain America",
+    photo:
+      "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj1i839o9jfAhWTCjQIHW0lDQQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.flickr.com%2Fphotos%2Ftales2astonish%2F6957191600&psig=AOvVaw2m5VWyI58VvvFrzAzq4G2z&ust=1546833991677520",
+    score: ["1", "4", "2", "5", "5", "3", "2", "1", "5", "1"]
+  },
+  {
+    name: "Thor",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fc1.staticflickr.com%2F4%2F3838%2F14217863550_cfae690f21_b.jpg&imgrefurl=https%3A%2F%2Fwww.flickr.com%2Fphotos%2F124561666%40N02%2F14217863550&docid=B74JwVYqSn0TKM&tbnid=k-dWjk8kRgLNTM%3A&vet=10ahUKEwi28s2ppNjfAhXjGjQIHfdeBAMQMwhtKAMwAw..i&w=1024&h=576&bih=889&biw=1920&q=thor&ved=0ahUKEwi28s2ppNjfAhXjGjQIHfdeBAMQMwhtKAMwAw&iact=mrc&uact=8",
+    score: ["3", "1", "4", "4", "5", "2", "2", "1", "5", "3"]
+  },
+  {
+    name: "Black Widow",
+    photo:
+      "https://www.google.com/imgres?imgurl=https%3A%2F%2Fc1.staticflickr.com%2F8%2F7095%2F7187991845_192b077bbe_c.jpg&imgrefurl=https%3A%2F%2Fwww.flickr.com%2Fphotos%2Ftales2astonish%2F7187991845&docid=zOEYoYGX3z2L6M&tbnid=KEcFqNS36fsRoM%3A&vet=12ahUKEwirh9vTpNjfAhX9IDQIHS9oCoI4ZBAzKF8wX3oECAEQYA..i&w=600&h=800&bih=889&biw=1920&q=black%20widow&ved=2ahUKEwirh9vTpNjfAhX9IDQIHS9oCoI4ZBAzKF8wX3oECAEQYA&iact=mrc&uact=8",
+    score: ["1", "4", "2", "2", "3", "1", "4", "4", "2", "4"]
+  }
+];
+
+module.exports = friends;
